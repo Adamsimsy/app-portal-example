1380bbe4364d00100c99481b28fc36eb0f070927
Adam Simmonds <adam.simmonds@avanade.com>
2021-10-04 21:39:00 +0100

    Menu items added

--- src/components/PortalAppBar.tsx
+++ src/components/PortalAppBar.tsx
@@ -3,23 +3,59 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Box from '@mui/material/Box';
 
-export class PortalAppBar extends Component<{}> {
+export class PortalAppBar extends Component<{}, { isDrawerOpen: boolean }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isDrawerOpen: false
+    };
+  }
+
   render() {
     return (
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+      <div>
+        <Drawer
+          open={this.state.isDrawerOpen}
+          onClose={() => this.setState({ isDrawerOpen: false })}
+        >
+          <Box
+            sx={{ width: 250, bgcolor: 'primary.dark' }}>
+            <List>
+              <ListItem button>
+                <ListItemText primary="Home" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="About" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Contact" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Services" />
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={() => this.setState({ isDrawerOpen: true })} >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar >
+      </div>
     );
   }
 }
