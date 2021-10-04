35c4dfc5e7af4097713b50c148798f75db25383f
Adam Simmonds <adam.simmonds@avanade.com>
2021-10-04 20:14:00 +0100

    Added material ui app bar

--- src/components/PortalAppBar.tsx
+++ src/components/PortalAppBar.tsx
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export class PortalAppBar extends Component<{}> {
-    render() {
-        return <p>Test menu</p>
-      }
+  render() {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
